78e51683740bc27c52a96aaec4c36134c717bb39
Sukhbir Brar <sukhmanbrar1510@gmail.com>
2019-11-07 23:14:23 -0600

    ActiveAdmin and Active_admin resources

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   get 'products/index'
   root 'products#index'
+
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
