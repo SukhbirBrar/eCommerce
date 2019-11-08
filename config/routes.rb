dc26947c1a62eda5567f311919640b9ccb28d8ed
Sukhbir Brar <sukhmanbrar1510@gmail.com>
2019-11-07 21:41:52 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  get 'products/index'
+  root 'products#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
